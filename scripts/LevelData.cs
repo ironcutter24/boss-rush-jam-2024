69a88fb22e75308d79046f262592366bc11e1d2c
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-01-09 17:52:31 +0100

    feat: determination of reachable grid cells

--- scripts/LevelData.cs
+++ scripts/LevelData.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class LevelData : Node3D
 {
@@ -15,12 +16,12 @@ public partial class LevelData : Node3D
     /*
      * Grid in game world:
      * 
-     * .^ (Row / i / z)
+     * .^ (Row / i / x)
      * .|
      * 2|
      * 1|
      * 0|
-     *  |----------> (Column / j / x)
+     *  |----------> (Column / j / z)
      *   0 1 2 3 ...
      */
     public CellData[,] Level = new CellData[NUM_OF_ROWS, NUM_OF_COLS];
@@ -31,14 +32,8 @@ public partial class LevelData : Node3D
     {
         InitAStar(navGrid);
 
-        const int start = 4;
-        const int end = 27;
-        GD.Print($"Path from {start} to {end}");
-        var testPath = navGrid.GetPointPath(start, end);
-        foreach(var point in testPath)
-        {
-            GD.Print(point);
-        }
+        //DebugPrintPath(17, 29);
+        //DebugPrintReachables(19, 3);
     }
 
     private void InitAStar(AStar3D aStar)
@@ -48,7 +43,7 @@ public partial class LevelData : Node3D
             for (int j = 0; j < NUM_OF_COLS; j++)
             {
                 var worldPos = GetWorldPos(i, j);
-                if (!HasColliderAt(worldPos))
+                if (!HasObstacleAt(worldPos))
                 {
                     AddPoint(aStar, i, j, worldPos);
                     InitConnections(aStar, i, j);
@@ -83,17 +78,60 @@ public partial class LevelData : Node3D
         }
     }
 
+    private static List<int> GetReachableIds(AStar3D aStar, int id, int maxDist)
+    {
+        var reachable = new List<int>();
+        foreach (int testId in GetBoundIds(id, maxDist))
+        {
+            if (aStar.HasPoint(testId) && aStar.GetIdPath(id, testId).Length - 1 <= maxDist)
+            {
+                reachable.Add(testId);
+            }
+        }
+        return reachable;
+
+
+        static List<int> GetBoundIds(int id, int maxDist)
+        {
+            var (x, z) = GetIndexes(id);
+            var bounds = new List<int>();
+            for (int i = 0; i < NUM_OF_ROWS; i++)
+            {
+                for (int j = 0; j < NUM_OF_COLS; j++)
+                {
+                    if (i == x && j == z) continue;
+                    if (Mathf.Abs(x - i) <= maxDist && Mathf.Abs(z - j) <= maxDist)
+                    {
+                        bounds.Add(GetId(i, j));
+                    }
+                }
+            }
+            return bounds;
+        }
+    }
+
     private static int GetId(int i, int j)
     {
         return NUM_OF_COLS * i + j;
     }
 
-    private static Vector3 GetWorldPos(int i, int j)
+    private static (int i, int j) GetIndexes(int id)
     {
-        return new Vector3(j, 0f, i) * CELL_SIZE;
+        return (id / NUM_OF_COLS, id % NUM_OF_COLS);
     }
 
-    private bool HasColliderAt(Vector3 pos)
+    private Vector3 GetWorldPos(int id)
+    {
+        var (i, j) = GetIndexes(id);
+        return GetWorldPos(i, j);
+    }
+
+    private Vector3 GetWorldPos(int i, int j)
+    {
+        return GlobalPosition + new Vector3(j, 0f, i) * CELL_SIZE;
+    }
+
+    private bool HasObstacleAt(Vector3 pos)
     {
         ray.Position = pos;
         ray.ForceRaycastUpdate();
@@ -102,12 +140,41 @@ public partial class LevelData : Node3D
 
     private void SpawnSphere(Vector3 pos, float scale)
     {
-        var packedScene = GD.Load<PackedScene>("res://scenes/sphere.tscn");
+        var packedScene = GD.Load<PackedScene>("res://scenes/debug/sphere.tscn");
         var shapeInstance = packedScene.Instantiate() as Node3D;
         AddChild(shapeInstance);
         shapeInstance.Position = pos;
         shapeInstance.Scale = Vector3.One * scale;
     }
+
+    #region Debug methods
+
+    private void DebugPrintPath(int start, int end)
+    {
+        SpawnSphere(GetWorldPos(start), .6f);
+        SpawnSphere(GetWorldPos(end), .6f);
+
+        GD.Print($"Path from {start} to {end}");
+        var path = navGrid.GetPointPath(start, end);
+        foreach (var point in path)
+        {
+            GD.Print(point);
+            SpawnSphere(point, .4f);
+        }
+    }
+
+    private void DebugPrintReachables(int id, int maxDist)
+    {
+        SpawnSphere(GetWorldPos(id), .6f);
+        foreach (var testId in GetReachableIds(navGrid, id, maxDist))
+        {
+            var (i, j) = GetIndexes(testId);
+            var pos = GetWorldPos(i, j);
+            SpawnSphere(pos, .4f);
+        }
+    }
+
+    #endregion
 
     public class CellData
     {
