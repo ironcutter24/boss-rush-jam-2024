552530a988ebfc1fb7bb161653d14e63b6845f10
ironcutter24 <ironcutter24@gmail.com>
2024-01-30 20:55:13 +0100

    chore: imported particle sprites assets

--- scripts/HUD/UnitHUD.cs
+++ scripts/HUD/UnitHUD.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class UnitHUD : Panel
 {
+    const string reactionFooter = "\n\n[i]Reactions can be used after swapping with an attacked unit, and expire at the beginning of player's turn.[/i]";
+
     [Export] private ActionInfo attackInfo;
     [Export] private ActionInfo specialInfo;
     [Export] private ActionInfo reactionInfo;
@@ -18,6 +20,6 @@ public partial class UnitHUD : Panel
     {
         attackInfo.SetContent("[b]Attack[/b]", unit.AttackDescription);
         specialInfo.SetContent("[b]Special[/b]", unit.SpecialDescription);
-        reactionInfo.SetContent("[b]Reaction[/b]", unit.ReactionDescription);
+        reactionInfo.SetContent("[b]Reaction[/b]", unit.ReactionDescription + reactionFooter);
     }
 }
